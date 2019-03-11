bbbce00b65d643ceb166b398d6f754707553e346
Jaafar Skafi <jaafarskafi1@gmail.com>
2019-03-11 14:44:12 -0700

    add argument for number of sentences returned

--- auto.js
+++ auto.js
@@ -1,22 +1,24 @@
 const axios = require('axios');
 const env = require('dotenv');
 const path = require('path');
-env.config({path: `${__dirname}/.env`});
+env.config({ path: `${__dirname}/.env` });
 
-const summarize = async (url) => {
-
+const summarize = async (url, numberOfSentences) => {
+  numberOfSentences = numberOfSentences || 7;
   try {
-    const response = await axios.get(`https://api.smmry.com/?SM_API_KEY=${process.env.API_KEY}&SM_URL=${url}`)
+    const response = await axios.get(`https://api.smmry.com/?SM_API_KEY=${process.env.API_KEY}&SM_LENGTH=${numberOfSentences}&SM_URL=${url}`)
     if (response.data.sm_api_error > 0) {
       throw new Error(`response returned an error: ${response.data.sm_api_message}`);
     } else {
       console.log(response.data.sm_api_content);
+      return response.data.sm_api_content;
     }
   } catch (err) {
     console.log(err);
   }
 }
 
-let input = process.argv.slice(2).join(' ');
+let url = process.argv[2];
+let numberOfSentences = process.argv[3];
 
-summarize(input);
+summarize(url, numberOfSentences);
